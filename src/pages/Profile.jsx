d147d5a3fb046dad76f237168bba1b83ce98c5c2
durga79 <91417489+durga79@users.noreply.github.com>
2023-06-13 11:49:35 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,32 +1,18 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { Context } from "../main";
+import Loader from "../components/Loader";
 
-const AboutPage = () => {
-  return (
+const Profile = () => {
+  const { isAuthenticated, loading, user } = useContext(Context);
+
+  return loading ? (
+    <Loader />
+  ) : (
     <div>
-      <h1>Welcome to [my Todo list]!</h1>
-      <p>
-        [Todo App ] is a powerful and intuitive todo list application designed to help you stay organized, manage your tasks, and boost your productivity. Whether you're juggling personal goals, work projects, or daily chores, our app provides the tools you need to streamline your workflow and conquer your to-do list.
-      </p>
-
-      <h2>Key Features:</h2>
-      <ol>
-        <li>Easy Task Management: Create, edit, and organize tasks effortlessly. Prioritize your tasks, set due dates, and assign categories to ensure nothing slips through the cracks.</li>
-        <li>Reminders and Notifications: Stay on top of your responsibilities with timely reminders and push notifications. Never miss an important deadline again.</li>
-        <li>Collaborative Features: Collaborate with friends, family, or colleagues by sharing task lists. Delegate tasks, track progress, and communicate effectively within the app.</li>
-        <li>Customization Options: Tailor the app to your preferences. Personalize your task lists with different colors, icons, and themes. Customize notifications to suit your workflow.</li>
-        <li>Sync Across Devices: Access your tasks from anywhere, anytime. Our app syncs seamlessly across multiple devices, ensuring your to-do lists are always up to date.</li>
-        <li>Productivity Insights: Gain valuable insights into your task completion patterns and productivity trends. Analyze your progress over time and make data-driven decisions to optimize your workflow.</li>
-      </ol>
-
-      <p>
-        [Your Todo App Name] is designed with a clean and intuitive interface, making it easy to navigate and use. We believe in simplicity without compromising functionality, allowing you to focus on what matters most - getting things done.
-      </p>
-
-      <p>
-        Download [Your Todo App Name] now and take control of your tasks. Let us help you achieve your goals and unlock your full potential!
-      </p>
+      <h1>{user?.name}</h1>
+      <p>{user?.email}</p>
     </div>
   );
 };
 
-export default AboutPage;
+export default Profile;
